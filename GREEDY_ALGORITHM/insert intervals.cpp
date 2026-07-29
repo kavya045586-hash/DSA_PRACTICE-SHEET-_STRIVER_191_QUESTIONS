4f5cce8298c558417f2a47a43bc62644f37c675f
Kavya K.N <kavya045586@gmail.com>
2026-07-29 13:49:48 +0530

    DSA

---
+++ GREEDY_ALGORITHM/insert intervals.cpp
@@ -0,0 +1,54 @@
+/*
+LeetCode Problem: Insert Interval
+
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] 
+represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. 
+You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Two intervals are considered overlapping if they share at least one point.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti 
+and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+Example 1:
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+Example 2:
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+*/
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int i = 0;
+        int n = intervals.size();
+        vector<vector<int>> r;
+
+        // Step 1: Add intervals before newInterval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            r.push_back(intervals[i]);
+            i++;
+        }
+
+        // Step 2: Merge overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(intervals[i][0], newInterval[0]);
+            newInterval[1] = max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+        r.push_back(newInterval);
+
+        // Step 3: Add remaining intervals
+        while (i < n) {
+            r.push_back(intervals[i]);
+            i++;
+        }
+
+        return r;
+    }
+};
